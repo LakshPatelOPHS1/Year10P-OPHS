d5c38d8ff280d9e9030982f769273a43ef0b785c
LakshOPHS <68946802+LakshPatelOPHS1@users.noreply.github.com>
2020-09-22 10:08:31 +0100

    Add files via upload

---
+++ 07 - String Slicing 02.py
@@ -0,0 +1,7 @@
+f_name = str(input("What is ur first name"))
+s_name = str(input("What is ur last name"))
+
+whole_name = f_name + " " + s_name
+
+print(whole_name)
+print(len(whole_name))
---
+++ 07 - String Slicing 03.py
@@ -0,0 +1,4 @@
+f_name = str(input("Enter your first name ")).lower()
+s_name = str(input("Enter your surname ")).lower()
+
+print(f_name + " " + s_name)
